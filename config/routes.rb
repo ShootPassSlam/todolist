9e1235a69807c0827a9718d6f20c3885472872b7
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2019-11-11 11:14:53 -0800

    testing explicit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :todos, path: "/"
-  delete "/" => "todos#destroy_all"
+  # resources :todos, path: "/"
+  # delete "/" => "todos#destroy_all"
+
+    post "/" => "todos#create"
+    delete "/" => "todos#destroy_all"
+    get "/:id" => "todos#show"
+    patch "/:id" => "todos#update"
+    delete "/:id" => "todos#destroy"
+    root 'todos#index'
 end
